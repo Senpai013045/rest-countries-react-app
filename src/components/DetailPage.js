a3a65cc1f3d590ca4c1431c1b3865dcf6e835c41
Senpai013045 <Subham.hero01@gmail.com>
2020-12-31 23:14:28 +0300

    url paramemters are now grabbable

--- src/components/DetailPage.js
+++ src/components/DetailPage.js
@@ -1,10 +1,15 @@
 import React from "react";
-import { useHistory } from "react-router";
+import { Redirect, useHistory, useParams } from "react-router";
 import Button from "./Button";
 import styles from "./DetailPage.module.css";
 
 const DetailPage = (props) => {
   const history = useHistory();
+  const { countryName } = useParams();
+
+  if (!countryName) {
+    return <Redirect to="/" />;
+  }
 
   return (
     <section
