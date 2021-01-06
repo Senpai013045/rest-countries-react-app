b4782e458f3eae203d612201d124d2c642907470
Senpai013045 <Subham.hero01@gmail.com>
2021-01-06 21:09:11 +0300

    clicking header now resets the search text and dropdown as well

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import ThemeSwitcher from "./ThemeSwitcher";
 import styles from "./Header.module.css";
+import { useHistory } from "react-router";
+import GlobalContext from "../context/globalContext";
 
-const Header = (props) => {
+const Header = () => {
+  const history = useHistory();
+  const { setSelected, setFilterText } = useContext(GlobalContext);
+
+  const clickHandler = () => {
+    setSelected("All");
+    setFilterText("");
+    history.push("/");
+  };
   return (
     <header className={styles.header}>
-      <h1 className={styles.primaryHeading}>Where in the world?</h1>
+      <h1 className={styles.primaryHeading} onClick={clickHandler}>
+        Where in the world?
+      </h1>
       <ThemeSwitcher />
     </header>
   );
